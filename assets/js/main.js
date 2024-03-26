7b0f05ffd32392f3ccbafd89d92b0c476600fcaa
Gigllyan <164183183+Gigllyan@users.noreply.github.com>
2024-03-25 21:36:25 -0300

    Atualizar o main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,50 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input, .contact__button',{interval: 200}); 
+
+
+
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Get all elements with class "skills__data"
+var skillItems = document.querySelectorAll(".skills__data");
+
+// Loop through each skill item
+skillItems.forEach(function(item) {
+  // Add click event listener to each skill item
+  item.addEventListener("click", function() {
+    // Get data from the data attribute
+    var dados = this.getAttribute("data-dados");
+    var titulo = this.querySelector(".skills__name").innerText;
+    var modalContent = document.getElementById("modalContent");
+    var modalTitle = document.getElementById("modalTitle");
+
+    // Set modal content dynamically
+    modalContent.innerHTML = dados;
+    modalTitle.innerText = titulo;
+
+    // Display the modal
+    modal.style.display = "block";
+  });
+});
