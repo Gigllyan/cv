26522b69608e4ae1e298574c1ce4d561a1abeb50
Gigllyan <164183183+Gigllyan@users.noreply.github.com>
2024-04-24 20:00:38 -0300

    Add files via upload

---
+++ projetos/readfromfolder/script.js
@@ -0,0 +1,27 @@
+// Get the folder path
+const folderPath = 'files/';
+
+// Get the file list
+const files = [];
+fetch(folderPath)
+  .then(response => response.json())
+  .then(data => {
+    files = data;
+    const fileList = document.getElementById('file-list');
+    files.forEach(file => {
+      const li = document.createElement('li');
+      li.textContent = file.name;
+      fileList.appendChild(li);
+    });
+  });
+
+// Read the contents of each file
+files.forEach(file => {
+  const fileReader = new FileReader();
+  fileReader.onload = event => {
+    const fileContent = event.target.result;
+    // Do something with the file content
+    console.log(fileContent);
+  };
+  fileReader.readAsText(file);
+});
